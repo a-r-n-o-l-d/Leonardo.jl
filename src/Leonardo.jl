1ae4242a99f6d36130fe176bcc389771a88e0a16
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-10-12 20:53:09 +0200

    includes

--- src/Leonardo.jl
+++ src/Leonardo.jl
@@ -1,5 +1,12 @@
 module Leonardo
 
-# Write your package code here.
+include("singletons.jl")
+include("charset.jl")
+include("printstyle.jl")
+include("canvas.jl")
+include("lines.jl")
+include("connectors.jl")
+include("boxes.jl")
+include("texts.jl")
 
 end
