51fe9ae8d286e6be21ba5663fc2bbf13f071c124
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-10-12 20:05:16 +0200

    Canvas

--- src/canvas.jl
+++ src/canvas.jl
@@ -16,3 +16,35 @@ function PrintStyle(; bold = false, color = :normal, blink = false, reverse = fa
  @inline function charprint(io::IO, c::Char, ps::PrintStyle = DEFAULT_PSTYLE)
     printstyled(io, c; ps.style...)
  end
+
+ struct Canvas
+    chars
+    pstyles
+end
+
+function Canvas(; width, height)
+    Canvas(_emptychars(width, height), fill(DEFAULT_PSTYLE, width + 1, height))
+end
+
+function reset!(canvas)
+    width, height = size(canvas.chars)
+    width = width - 1
+    copyto!(canvas.chars, _emptychars(width, height))
+    fill!(canvas.crayons, Crayon())
+    canvas
+end
+
+@inline function _emptychars(width, height)
+    line = vcat(repeat([' '], width), '\n')
+    reshape(repeat(line, height), width + 1, height)
+end
+
+function Base.print(io::IO, canvas::Canvas)
+    for (ch, ps) in zip(canvas.chars, canvas.pstyles)
+        charprint(io, ch, ps)
+    end
+end
+
+function Base.show(io::IO, canvas::Canvas)
+    print(io, canvas)
+end
